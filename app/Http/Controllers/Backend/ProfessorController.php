bee456bca82b689daddeb8f11b8880badb104ec6
OungHongly <ounghonly.9999@gmail.com>
2023-06-29 02:24:10 +0700

    show-data-table-teacher

--- app/Http/Controllers/Backend/ProfessorController.php
+++ app/Http/Controllers/Backend/ProfessorController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Teacher;
+use DB;
 
 class ProfessorController extends Controller
 {
     public function index(){
-        return view('backend/admin/Professor.index');
+        //$teacher_arr = DB::table('users')->get();
+        //dd($teacher_arr);
+        $professor = DB::select('SELECT * from users where role_id=7'); 
+        return view('backend/admin/Professor.index',['users' => $professor]);
     }
 }
